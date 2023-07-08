4c16153fbfefeac517a3b65f79031b982266fb70
Ambani M. Elphas <137505343+ambanimelphas@users.noreply.github.com>
2023-07-08 20:17:56 +0300

    Update MycoolappApplication.java

--- 01-spring-boot-overview/01-spring-boot-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/MycoolappApplication.java
+++ 01-spring-boot-overview/01-spring-boot-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/MycoolappApplication.java
@@ -3,7 +3,7 @@ package com.luv2code.springboot.demo.mycoolapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplicatio
+@SpringBootApplication
 public class MycoolappApplication {
 
 	public static void main(String[] args) {
